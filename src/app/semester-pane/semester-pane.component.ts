145846532e229d13e88db2676a42739ccb21a95c
Bryan <bryanwu123456@gmail.com>
2019-12-03 17:37:17 -0800

    app passes info to semesterpane. semesterpane stores semester info/classes.

--- src/app/semester-pane/semester-pane.component.ts
+++ src/app/semester-pane/semester-pane.component.ts
@@ -7,9 +7,12 @@ import { Component, OnInit, Input} from '@angular/core';
 })
 export class SemesterPaneComponent implements OnInit {
   temp_semester_list = {//format: "season year": {num_classes, list_classes}
-    "Fall 2019": [11111, 11112, 11113]
+    "Fall 2019": [11111, 11112, 11113],
+    "Spring 2020" : [11112, 11113,11113]
   }
   @Input() registered_courses;
+  //TODO: if importing takes up extra space, it may be worth just using export
+  // instead to find the relevant classes so that we don't store copies
 
   constructor() { }
 
