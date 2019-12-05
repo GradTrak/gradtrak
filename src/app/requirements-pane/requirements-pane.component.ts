3109d31ccb777f544501710b03e9013fd3f6c294
Nicholas Ngai <ngai.nicholas@protonmail.com>
2019-12-04 22:58:33 -0800

    Add requirement service

--- src/app/requirements-pane/requirements-pane.component.ts
+++ src/app/requirements-pane/requirements-pane.component.ts
@@ -1,74 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 import { RequirementSet } from '../requirement-set'
+import { RequirementService } from '../requirement.service';
 
 @Component({
   selector: 'app-requirements-pane',
   templateUrl: './requirements-pane.component.html',
   styleUrls: ['./requirements-pane.component.css']
 })
-export class RequirementsPaneComponent implements OnInit {
-  requirementSets: RequirementSet[] = [
-    {
-      id: 'coe',
-      name: 'College of Engineering',
-      parent: null,
-      requirementCategories: [
-        {
-          id: 'math',
-          name: 'Math',
-          requirements: [
-            {
-              id: 'math1a',
-              name: 'MATH 1A',
-            },
-            {
-              id: 'math1b',
-              name: 'MATH 1B',
-            },
-          ],
-        },
-        {
-          id: 'physics',
-          name: 'Physics',
-          requirements: [
-            {
-              id: 'physics7a',
-              name: 'PHYSICS 7A',
-            },
-            {
-              id: 'physics7b',
-              name: 'PHYSICS 7B',
-            },
-          ],
-        },
-      ],
-    },
-    {
-      id: 'eecs',
-      name: 'EECS Major',
-      parent: null,
-      requirementCategories: [
-        {
-          id: 'eecs-lower-div',
-          name: 'Lower Division Requirements',
-          requirements: [
-            {
-              id: 'cs61a',
-              name: 'CS 61A',
-            },
-            {
-              id: 'cs61b',
-              name: 'CS 61B',
-            },
-          ],
-        },
-      ],
-    },
-  ];
 
-  constructor() { }
+export class RequirementsPaneComponent implements OnInit {
+  requirementSets: RequirementSet[];
+
+  constructor(private requirementService: RequirementService) { }
 
   ngOnInit() {
+    this.requirementService.getRequirements()
+      .subscribe(requirementSets => {
+        this.requirementSets = requirementSets;
+      });
   }
-
 }
