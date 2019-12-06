65e5bd1272dff83fea2dd6ea6eb0aae97d3add01
Bryan <bryanwu123456@gmail.com>
2019-12-06 14:41:44 -0800

    used services to connect all class and semseters

--- src/app/semester-pane/semester-pane.component.ts
+++ src/app/semester-pane/semester-pane.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, Input} from '@angular/core';
 import {Semester} from '../semester';
 import {Course} from '../course';
 import {CourseService} from '../course.service';
+import {SemesterService} from './semester.service'
 
 @Component({
   selector: 'app-semester-pane',
@@ -9,44 +10,18 @@ import {CourseService} from '../course.service';
   styleUrls: ['./semester-pane.component.css']
 })
 export class SemesterPaneComponent implements OnInit {
-  registeredCourses: Course[] = [];
   test = "string";
   //TODO: if importing takes up extra space, it may be worth just using export
   // instead to find the relevant classes so that we don't store copies
 
-  semesterList: Semester[] = [ {
-      id: "fa19",
-      name: "Fall 2019",
-      courses: [
-        {id : "cs61a",
-        dept : "CS",
-        no: "61a",
-        title: "str int cmp pgrms",
-        units: 4
-      },
-      {id : "cs61b",
-      dept : "CS",
-      no: "61b",
-      title: "Data structures",
-      units: 4
-      }
-    ],
-  },
-    {
-      id: "sp20",
-      name: "Spring 2020",
-      courses: [
-        //this.registeredCourses[0]
-        //this doesn't work because it's a class variable and stuff.
-      ],
-    },
-  ];
+  semesterList: Semester[] = [];
 
-  constructor(private _courseService : CourseService) {
+  constructor(private _courseService : CourseService, private _semesterService:SemesterService) {
   };
 
   ngOnInit() {
-    this.registeredCourses = this._courseService.getCourses();
+    this.semesterList = this._semesterService.getSemesters();
+    //this.test = this.semesterList[1].courses[0].id
   };
 
 }
--- src/app/semester-pane/semester.service.ts
+++ src/app/semester-pane/semester.service.ts
@@ -1,10 +1,43 @@
 import { Injectable } from '@angular/core';
-import {CourseService} from '../../course.service'
+import {CourseService} from '../course.service';
+import {Course} from '../course'
+import {Semester} from '../semester';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SemesterService {
-
-  constructor() { }
+  registeredCourses: Course[] = [];
+  constructor(private _courseService: CourseService) { }
+  getSemesters(): Semester[]{
+    this.registeredCourses = this._courseService.getCourses();
+    return [ {
+        id: "fa19",
+        name: "Fall 2019",
+        courses: [
+          this.registeredCourses[0],
+          this.registeredCourses[1],
+      ],
+    },
+      {
+        id: "sp20",
+        name: "Spring 2020",
+        courses: [
+          this.registeredCourses[2],
+          this.registeredCourses[2],
+          this.registeredCourses[2],
+          this.registeredCourses[2],
+          this.registeredCourses[2],
+          this.registeredCourses[2],
+          this.registeredCourses[2],
+          this.registeredCourses[2],
+          this.registeredCourses[2],
+          this.registeredCourses[2],
+          this.registeredCourses[2],
+          this.registeredCourses[2],
+          
+        ],
+      },
+    ];
+  }
 }
