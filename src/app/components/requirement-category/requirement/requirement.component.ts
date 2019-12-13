8ef157fe9f295ea187a0c0e70f84d6e7db5b12c0
Nicholas Ngai <ngai.nicholas@protonmail.com>
2019-12-13 09:45:16 -0800

    Inherit requirement styles from requirement-category

--- src/app/components/requirement-category/requirement/requirement.component.ts
+++ src/app/components/requirement-category/requirement/requirement.component.ts
@@ -4,7 +4,7 @@ import { Requirement } from 'models/requirement.model';
 @Component({
   selector: '[app-requirement]',
   templateUrl: './requirement.component.html',
-  styleUrls: ['./requirement.component.css'],
+  styleUrls: ['./requirement.component.css', '../requirement-category.component.css'],
 })
 export class RequirementComponent implements OnInit {
   @Input() requirement: Requirement;
