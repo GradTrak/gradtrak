0e0f4e3ac9af6d0b71c13a495d4dd54815cddc7d
Nicholas Ngai <ngai.nicholas@protonmail.com>
2019-12-17 21:13:39 -0800

    Ignore node_modules and rename compiled client assets to dist in server folder

--- server/app.js
+++ server/app.js
@@ -10,6 +10,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-app.use(express.static('client'));
+app.use(express.static('dist'));
 
 module.exports = app;
