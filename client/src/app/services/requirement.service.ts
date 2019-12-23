3a286ae51a803c114fa76bcc8da1b4bd5ab95632
Nicholas Ngai <ngai.nicholas@protonmail.com>
2019-12-23 14:35:07 -0800

    Fix COMPSCI 61C requirement name

--- client/src/app/services/requirement.service.ts
+++ client/src/app/services/requirement.service.ts
@@ -205,7 +205,7 @@ export class RequirementService {
             },
             {
               id: 'compsci61c',
-              name: 'COMPSCI 61A',
+              name: 'COMPSCI 61C',
             },
             {
               id: 'eecs16a',
