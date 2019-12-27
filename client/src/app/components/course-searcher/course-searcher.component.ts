a36b2c2d1e387f5ed9a10f87a6b23ce142cfbda0
Bryan Wu <bryanwu123456@gmail.com>
2019-12-26 18:03:57 -0800

    add search function and display searched courses

--- client/src/app/components/course-searcher/course-searcher.component.ts
+++ client/src/app/components/course-searcher/course-searcher.component.ts
@@ -10,13 +10,27 @@ import { CourseService } from 'services/course.service';
 })
 export class CourseSearcherComponent implements OnInit {
   searchPhrase: FormControl = new FormControl();
-  courseMatches: Course[]; //uses courseIDs so that the courseID is displayed
-  //in the text box, not [object, object]
+  allCourses: Course[];
+  courseMatches: Course[];
+  testbool;
   constructor(private courseService: CourseService) { }
 
+  updateAutoComplete():void{
+    function searchFunction(input: string, courseList: Course[]): Course[] {
+      return courseList.filter((course)=>{
+        return (
+          course.id.includes(input) ||
+          course.title.includes(input) ||
+          course.dept.includes(input) ||
+          course.no.includes(input))
+      })
+    }
+    this.courseMatches = searchFunction(this.searchPhrase.value, this.allCourses)
+  }
   ngOnInit() {
     this.courseService.getCourses().subscribe((courses: Course[])=>{
-      this.courseMatches = courses
+      this.allCourses = courses;
+      this.courseMatches = courses;
     })
   }
 
