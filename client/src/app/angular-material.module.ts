ab6506ad3a74b1bc675f109112d59a1d12b751b3
Bryan Wu <bryanwu123456@gmail.com>
2019-12-26 17:11:12 -0800

    import course model and service for autocomplete, add angular material, and create autocomplete based on courses

---
+++ client/src/app/angular-material.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import {
+    MatAutocompleteModule,
+    MatInputModule
+} from '@angular/material';
+
+@NgModule({
+    imports: [MatAutocompleteModule,MatInputModule],
+    exports: [MatAutocompleteModule,MatInputModule]
+})
+
+export class AngularMaterialModule { }
--- client/src/app/components/course-searcher/course-searcher.component.ts
+++ client/src/app/components/course-searcher/course-searcher.component.ts
@@ -1,16 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
+import { Course } from 'models/course.model';
+import { CourseService } from 'services/course.service';
+
 @Component({
   selector: 'app-course-searcher',
   templateUrl: './course-searcher.component.html',
   styleUrls: ['./course-searcher.component.css']
 })
 export class CourseSearcherComponent implements OnInit {
-  myControl = new FormControl();
-  options: string[] = ['61a', '61b', '70'];
-  constructor() { }
+  searchPhrase: FormControl = new FormControl();
+  courseMatches: Course[]; //uses courseIDs so that the courseID is displayed
+  //in the text box, not [object, object]
+  constructor(private courseService: CourseService) { }
 
   ngOnInit() {
+    this.courseService.getCourses().subscribe((courses: Course[])=>{
+      this.courseMatches = courses
+    })
   }
 
 
