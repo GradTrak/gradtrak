204d593aab95050c2cf405f16172a2274a7213fc
Bryan Wu <bryanwu123456@gmail.com>
2020-01-05 13:19:38 -0800

    put template into a modal and add test open/close buttons

--- client/src/app/components/requirements-pane/requirements-pane.component.ts
+++ client/src/app/components/requirements-pane/requirements-pane.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, TemplateRef } from '@angular/core';
 import { RequirementSet } from 'models/requirement-set.model';
 import { RequirementService } from 'services/requirement.service';
+import { NgbModal, NgbModalRef } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-requirements-pane',
@@ -11,6 +12,16 @@ export class RequirementsPaneComponent implements OnInit {
   private requirementSets: RequirementSet[];
   selectedRequirementSets: RequirementSet[];
   selectableRequirementSets: RequirementSet[];
+  @ViewChild('goalSelector', { static: false }) private goalSelectorTemplate: TemplateRef<any>; // eslint-disable-line @typescript-eslint/no-explicit-any
+  private modalInstance: NgbModalRef;
+
+  openSelector():void{
+    this.modalInstance = this.modalService.open(this.goalSelectorTemplate, { size: 'lg' })
+  }
+  closeSelector():void{
+    if (this.modalInstance){
+      this.modalInstance.close();}
+  }
 
   updateSelectedRequirements(baseReqSet: RequirementSet): RequirementSet[] {
     const selected: RequirementSet[] = [baseReqSet];
@@ -24,7 +35,7 @@ export class RequirementsPaneComponent implements OnInit {
     return selected;
   }
 
-  constructor(private requirementService: RequirementService) {}
+  constructor(private modalService: NgbModal, private requirementService: RequirementService) {}
 
   ngOnInit(): void {
     this.requirementService.getRequirements().subscribe((requirementSets) => {
