a2678daabc45ec9c1137a894278949f35c127513
Bryan Wu <bryanwu123456@gmail.com>
2020-01-05 13:04:48 -0800

    Add preliminary functions to goalService

--- client/src/app/services/goal-service.service.ts
+++ client/src/app/services/goal-service.service.ts
@@ -1,9 +1,26 @@
 import { Injectable } from '@angular/core';
-
+import { RequirementSet } from 'models/requirement-set.model';
+import { Observable, of } from 'rxjs';
+import { share } from 'rxjs/operators'
 @Injectable({
   providedIn: 'root'
 })
 export class GoalServiceService {
-
+  selectedGoals: Observable<RequirementSet[]>;
+  //Note that selectedGoals only has the Child selected goal.
+  //it will deliver the parents when needed
   constructor() { }
+  setGoals(goals: RequirementSet[]):void{
+    this.selectedGoals = of(goals).share();
+  }
+  getGoals(): Observable<RequirementSet[]> {
+    return this.selectedGoals;
+  }
+  addGoals(newGoal:RequirementSet):void{
+    this.selectedGoals = this.selectedGoals.pipe((goals: RequirementSet[])=>{
+      goals.push(newGoal);
+    },
+    share());
+  }
+
 }
