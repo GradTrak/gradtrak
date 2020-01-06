00e4fb542f625007b17d5cd9317b3132d071f119
Bryan Wu <bryanwu123456@gmail.com>
2020-01-05 16:24:48 -0800

    add debugging statements to find error

--- client/src/app/components/requirement-selector/requirement-selector.component.ts
+++ client/src/app/components/requirement-selector/requirement-selector.component.ts
@@ -9,12 +9,14 @@ import { GoalService } from 'services/goal.service'
 })
 export class RequirementSelectorComponent implements OnInit {
   searchPrompt: string;
-  searchedMajors: RequirementSet[];
+  searchedMajors: RequirementSet[];//feels like a lot of repeated code but again doesn't warrant a new component :(
   searchedMinors: RequirementSet[];
   searchedOthers: RequirementSet[];
+  selectedMajor: RequirementSet;
+  selectedMinor: RequirementSet;
+  selectedOther: RequirementSet;
   requirementSets: RequirementSet[];
   currentGoals: RequirementSet[];
-  test: any = "xothing changed so far";
   constructor(private requirementService: RequirementService, private goalService: GoalService) { }
 
   ngOnInit() {
@@ -26,8 +28,22 @@ export class RequirementSelectorComponent implements OnInit {
     //stuff beflow is for testing only
     this.goalService.setGoals([]);
     this.goalService.addGoals(this.requirementSets[1]);
+    this.updateCurrentGoals()
+    this.updateCurrentGoals()
+
+  }
+  private updateCurrentGoals():void{
+    alert(this.currentGoals)
+    this.goalService.getGoals()
+    alert(this.currentGoals)
     this.goalService.getGoals().subscribe((goalList: RequirementSet[])=>{
+      alert(this.currentGoals)
+      alert(goalList)
+      alert("setting current goals now")
       this.currentGoals = goalList;
+      alert(this.currentGoals)
+      alert(goalList)
+      alert ("end update")
     });
   }
 
@@ -48,6 +64,20 @@ export class RequirementSelectorComponent implements OnInit {
     this.searchedOthers = this.requirementSets.filter((potentialOther: RequirementSet)=>(potentialOther.type === 'other'&&
     this.searchFunction(this.searchPrompt, potentialOther)))
     //might make more sense to do the major minor sorting onInit only once and store it
+  }
+
+  addSelectedGoals(): void{
+    alert(this.currentGoals[0].id);
+    this.updateCurrentGoals()
+    alert(1)
+    alert(this.currentGoals[1].id);
+    this.selectedMajor?this.goalService.addGoals(this.selectedMajor):alert("major did not get added");
+    this.selectedMinor?this.goalService.addGoals(this.selectedMinor):alert("minor did not get added");
+    this.selectedOther?this.goalService.addGoals(this.selectedOther):alert("other did not get added");
+    this.updateCurrentGoals()
+    alert(this.currentGoals[0].id)
+    alert(this.currentGoals[1].id)
+    alert(this.currentGoals[2].id)
   }
 
 }
