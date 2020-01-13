f6d3f31b8e43fd9deb6abb17eb232da584290d28
Nicholas Ngai <ngai.nicholas@protonmail.com>
2020-01-12 20:53:17 -0800

    Lint code

--- server/app.js
+++ server/app.js
@@ -1,9 +1,8 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
-var app = express();
+const app = express();
 
 app.use(logger('dev'));
 app.use(express.json());
