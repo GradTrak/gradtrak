dcabd8d19685699b57fb58ae2b00c00fae1fb7a5
Nicholas Ngai <ngai.nicholas@protonmail.com>
2020-01-13 17:53:08 -0800

    Add dummy data to API controllers

--- server/controllers/semesterController.js
+++ server/controllers/semesterController.js
@@ -1,3 +1,46 @@
-exports.getSemesters = (req, res)=>{
-  res.send('you are retrieving semesters from the express app')//todo: replace with courses
-}
+const DUMMY_SEMESTER_DATA = {
+  fa2019: {
+    id: 'fa2019',
+    name: 'Fall 2019',
+    courses: [],
+  },
+  sp2020: {
+    id: 'sp2020',
+    name: 'Spring 2020',
+    courses: [],
+  },
+  fa20: {
+    id: 'fa20',
+    name: 'Fall 2020',
+    courses: [],
+  },
+  sp2021: {
+    id: 'sp2021',
+    name: 'Spring 2021',
+    courses: [],
+  },
+  fa21: {
+    id: 'fa21',
+    name: 'Fall 2021',
+    courses: [],
+  },
+  sp2022: {
+    id: 'sp2022',
+    name: 'Spring 2022',
+    courses: [],
+  },
+  fa22: {
+    id: 'fa22',
+    name: 'Fall 2022',
+    courses: [],
+  },
+  sp2023: {
+    id: 'sp2023',
+    name: 'Spring 2023',
+    courses: [],
+  },
+};
+
+exports.getSemesters = (req, res) => {
+  res.json(DUMMY_SEMESTER_DATA);
+};
--- server/routers/api.js
+++ server/routers/api.js
@@ -3,11 +3,13 @@ const express = require('express');
 const courseController = require('../controllers/courseController');
 const requirementController = require('../controllers/requirementController');
 const semesterController = require('../controllers/semesterController');
+const tagController = require('../controllers/tagController');
 
 const api = express.Router();
 
 api.use('/courses', courseController.getCourses);
 api.use('/semesters', semesterController.getSemesters);
 api.use('/requirements', requirementController.getRequirements);
+api.use('/tags', tagController.getTags);
 
 exports.api = api;
