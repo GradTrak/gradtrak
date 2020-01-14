2cff497f3000b804950850ddea4930fd0f2ebfd2
Bryan Wu <bryanwu123456@gmail.com>
2020-01-14 15:44:13 -0800

    add files that were left out last commit

---
+++ client/src/app/services/user.service.ts
@@ -0,0 +1,40 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { map, shareReplay } from 'rxjs/operators';
+import { Semester } from 'models/semester.model';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class UserService {
+  private static readonly SEMESTER_API_ENDPOINT = '/api/semesters';
+
+  private sharedSemestersObj: Observable<object>;
+
+  constructor(private http: HttpClient) {}
+
+  getSemesters(): Observable<Semester[]> {
+    if (!this.sharedSemestersObj) {
+      this.fetchSemesterData();
+    }
+    return this.sharedSemestersObj.pipe(map(Object.values));
+  }
+
+  getSemestersObj(): Observable<object> {
+    return this.sharedSemestersObj;
+  }
+
+  private fetchSemesterData(): void {
+    this.sharedSemestersObj = this.http
+      .get(UserService.SEMESTER_API_ENDPOINT)
+      .pipe(map(this.instantiateSemesters), shareReplay());
+  }
+
+  private instantiateSemesters(data: object): object {
+    Object.keys(data).forEach((key: string) => {
+      data[key] = new Semester(data[key]);
+    });
+    return data;
+  }
+}
