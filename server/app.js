cd968a8b9a258d3fdfb5765322d3dd4af8805263
Nicholas Ngai <ngai.nicholas@protonmail.com>
2020-01-13 17:21:41 -0800

    Rearrange imports and declarations

--- server/app.js
+++ server/app.js
@@ -1,18 +1,17 @@
 const express = require('express');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
-const app = express();
+
 const api = require('./routers/api').api;
 
-app.use('/api', api);
+const app = express();
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static('dist'));
 
-app.get('/', (req, res) => {
-  res.send('this is the express app');
-});
+app.use('/api', api);
+app.use(express.static('dist'));
 
 module.exports = app;
--- server/routers/api.js
+++ server/routers/api.js
@@ -1,11 +1,13 @@
 const express = require('express');
-const api = express.Router();
+
 const courseController = require('../controllers/courseController');
 const requirementController = require('../controllers/requirementController');
 const semesterController = require('../controllers/semesterController');
 
-api.get('/courses', courseController.getCourses);
-api.get('/semesters', semesterController.getSemesters);
-api.get('/requirements', requirementController.getRequirements);
+const api = express.Router();
 
-exports.api = api
+api.use('/courses', courseController.getCourses);
+api.use('/semesters', semesterController.getSemesters);
+api.use('/requirements', requirementController.getRequirements);
+
+exports.api = api;
