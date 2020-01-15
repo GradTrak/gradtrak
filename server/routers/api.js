c802e4e1da4a4213a34b5c44cd8808b11e72052c
Bryan Wu <bryanwu123456@gmail.com>
2020-01-14 17:17:38 -0800

    rename semester controller to usercontroller

--- server/routers/api.js
+++ server/routers/api.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const courseController = require('../controllers/courseController');
 const requirementController = require('../controllers/requirementController');
-const semesterController = require('../controllers/semesterController');
+const userController = require('../controllers/userController');
 const tagController = require('../controllers/tagController');
 const goalController = require('../controllers/goalController');
 
@@ -10,7 +10,7 @@ const api = express.Router();
 api.post('/goal', goalController.postGoals)
 api.get('/goal', goalController.getGoals);
 api.get('/courses', courseController.getCourses);
-api.get('/semesters', semesterController.getSemesters);
+api.get('/semesters', userController.getSemesters);
 api.get('/requirements', requirementController.getRequirements);
 api.use('/tags', tagController.getTags);
 
