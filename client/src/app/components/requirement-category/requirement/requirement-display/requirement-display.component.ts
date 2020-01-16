93965b00ef34b03289fac6cd28477f32fa60818e
Constance Shi <constance.shi@berkeley.edu>
2020-01-15 16:54:45 -0800

    prettified

---
+++ client/src/app/components/requirement-category/requirement/requirement-display/requirement-display.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-requirement-display',
+  templateUrl: './requirement-display.component.html',
+  styleUrls: ['./requirement-display.component.scss'],
+})
+export class RequirementDisplayComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
