4edc485b1f806e0f4a8bf8e96323dccb0e5850e4
Constance Shi <constance.shi@berkeley.edu>
2020-01-15 23:14:03 -0800

    filter through courses for unit requirements

--- client/src/app/components/requirement-category/requirement-display/requirement-display.component.ts
+++ client/src/app/components/requirement-category/requirement-display/requirement-display.component.ts
@@ -1,6 +1,9 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Course } from 'models/course.model';
 import { Requirement } from 'models/requirement.model';
+import { MutexRequirement } from 'models/requirements/mutex-requirement.model';
+import { UnitRequirement } from 'models/requirements/unit-requirement.model';
+import { TagRequirement } from 'models/requirements/tag-requirement.model';
 import { CourseService } from 'services/course.service';
 
 @Component({
@@ -19,7 +22,17 @@ export class RequirementDisplayComponent implements OnInit {
     this.courseService.getCourses().subscribe((courses: Course[]) => {
       this.allCourses = courses;
       this.requirementCourses = this.allCourses.filter((course) => {
-        return this.requirementInput.isFulfillableBy(course);
+        if (this.requirementInput instanceof TagRequirement) {
+          return this.requirementInput.isFulfillableBy(course);
+        }
+        if (this.requirementInput instanceof UnitRequirement) {
+          return this.requirementInput.requirement.isFulfillableBy(course);
+        }
+        if (this.requirementInput instanceof MutexRequirement) {
+          return this.requirementInput.requirements.filter((requirement) => {
+            requirement.isFulfillableBy(course);
+          });
+        }
       });
     });
   }
