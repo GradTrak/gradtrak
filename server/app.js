67900983f0110313e3860f5a4957aa5d54d11f28
Bryan Wu <bryanwu123456@gmail.com>
2020-01-19 16:00:08 -0800

    require mongoose

--- server/app.js
+++ server/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const mongoose = require('mongoose')
 
 const { api } = require('./routers/api');
 
@@ -13,5 +14,7 @@ app.use(cookieParser());
 
 app.use('/api', api);
 app.use(express.static('dist'));
+
+mongoose.connect('mongodb://55.55.55.5/mongo')
 
 module.exports = app;
