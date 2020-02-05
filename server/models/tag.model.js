5879bf5fc7456f7ff0f7d9614296d3b16c52338b
Constance Shi <constance.shi@berkeley.edu>
2020-02-04 23:21:43 -0800

    adjust schema for requirement and tag models

--- server/models/tag.model.js
+++ server/models/tag.model.js
@@ -7,26 +7,11 @@ const tagSchema = new Schema({
     required: true,
     unique: true,
   },
-  dept: {
+  name: {
     type: String,
     required: true,
     unique: true,
   },
-  no: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  title: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  units: {
-    type: Number,
-    required: true,
-  },
-  tags: Array,
 })
 
 module.exports = mongoose.model('tag', tagSchema)
