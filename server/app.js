a12eddfd393d56dae78fdf8ff6212ca8eee97fd9
Nicholas Ngai <ngai.nicholas@protonmail.com>
2020-02-13 12:46:36 -0800

    Install express-session and initialize Passport

--- server/app.js
+++ server/app.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const passport = require('passport');
+const passportLocal = require('passport-local');
+const session = require('express-session');
 
 const { api } = require('./routers/api');
 
@@ -10,6 +13,9 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(session({ secret: 'secret' }));
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use('/api', api);
 app.use(express.static('dist'));
