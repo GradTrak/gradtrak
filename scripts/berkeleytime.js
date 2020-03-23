d31fb2abb2a07c5dbefbf35d0951e03035628f69
Nicholas Ngai <ngai.nicholas@protonmail.com>
2020-03-23 13:27:35 -0700

    Construct course.dept, course.no, and course.id from fetched data

--- scripts/berkeleytime.js
+++ scripts/berkeleytime.js
@@ -3,11 +3,9 @@
 const fs = require('fs');
 const https = require('https');
 
-const LIST_ENDPOINT =
-  'https://berkeleytime.com/api/catalog/filter/?filters=22623,22626,32259,32036';
+const LIST_ENDPOINT = 'https://berkeleytime.com/api/catalog/filter/?filters=22623,22626,32259,32036';
 
-const COURSE_ENDPOINT =
-  'https://berkeleytime.com/api/catalog_json/course_box/?course_id=';
+const COURSE_ENDPOINT = 'https://berkeleytime.com/api/catalog_json/course_box/?course_id=';
 
 https.get(LIST_ENDPOINT, (res) => {
   let rawData = '';
@@ -19,15 +17,29 @@ https.get(LIST_ENDPOINT, (res) => {
   res.on('end', () => {
     const data = JSON.parse(rawData);
 
+    const validCourses = data.filter((course) => !course.units || course.units.match('^\\d+\\.\\d+$'));
+
     let i = 0;
     data.forEach((course) => {
       delete course.open_seats;
+      delete course.description;
       delete course.enrolled_percentage;
       delete course.favorite_count;
       delete course.waitlisted;
       delete course.enrolled;
       delete course.grade_average;
+      delete course.id;
       delete course.letter_average;
+
+      course.dept = course.abbreviation;
+      course.no = course.course_number;
+      delete course.abbreviation;
+      delete course.course_number;
+
+      course.id = course.dept.replace('\\s', '').toLowerCase() + course.no.replace('\\s', '').toLowerCase();
+
+      // Convert units to number
+      course.units = parseFloat(course.units);
 
       if (i < 1) {
         https.get(COURSE_ENDPOINT + course.id, (res) => {
@@ -46,9 +58,5 @@ https.get(LIST_ENDPOINT, (res) => {
         i++;
       }
     });
-
-    console.log(
-      data.filter((course) => !course.units || course.units.length != 3),
-    );
   });
 });
