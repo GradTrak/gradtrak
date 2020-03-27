86a5ace6fecdc4966171c03a2217beed9fc81a55
Nicholas Ngai <ngai.nicholas@protonmail.com>
2020-03-26 17:34:04 -0700

    Add populateFromBerkeleyTime.js

---
+++ scripts/populateFromBerkeleyTime.js
@@ -0,0 +1,43 @@
+#!/usr/bin/env node
+
+const fs = require('fs');
+
+const db = require('../server/config/db');
+
+const Course = require('../server/models/course');
+
+const DUMMY_DIR = 'dummy';
+
+const DUMMY_COURSE_DATA = JSON.parse(fs.readFileSync(`./${DUMMY_DIR}/course.json`));
+
+const dummyCourseMap = new Map();
+DUMMY_COURSE_DATA.forEach((course) => {
+  dummyCourseMap.set(course.id, course);
+});
+
+let conn;
+
+db.connect()
+  .then((c) => {
+    conn = c;
+    return Course.deleteMany({});
+  })
+  .then(() => {
+    const coursesJson = fs.readFileSync('./berkeleyTime.json');
+    const courses = JSON.parse(coursesJson);
+
+    // Include tags from the dummy data in course.json
+    courses.forEach((course) => {
+      if (dummyCourseMap.has(course.id)) {
+        course.tagIds = [...new Set([...course.tagIds, ...dummyCourseMap.get(course.id).tagIds])];
+      }
+    });
+
+    return Course.insertMany(courses);
+  })
+  .catch((err) => {
+    console.error(err);
+  })
+  .finally(() => {
+    conn.close();
+  });
