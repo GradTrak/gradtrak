c364f8f651995c44d505e363bfd8b806cebfaf86
Nicholas Ngai <ngai.nicholas@protonmail.com>
2020-04-13 14:00:21 -0700

    Fix spelling in comment

--- server/config/passport.js
+++ server/config/passport.js
@@ -5,7 +5,7 @@ const User = require('../models/user');
 
 async function verifyUser(user, inputPassword) {
   if (!user) {
-    /* Verify dummy password to prevent timing attack enumerating user */
+    /* Verify dummy password to prevent timing attack enumerating users */
     /* This is the hash of 'password' if anyone is curious */
     const dummy = '$argon2id$v=19$m=4096,t=3,p=1$bPaz0G0LK/r5aSoCQsU8Bg$8mbAff+svZA0QV3XH5i5vSPTYBE5xd4rmVZEDF0umvA';
     await argon2.verify(dummy, inputPassword);
