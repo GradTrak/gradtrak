3797928a9645ba330397deae6a615d3bb94f6428
Nicholas Ngai <ngai.nicholas@protonmail.com>
2020-05-24 13:51:07 -0700

    Add wildcard redirect to root

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -3,7 +3,10 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { PlannerComponent } from './planner/planner.component';
 
-const routes: Routes = [{ path: '', component: PlannerComponent }];
+const routes: Routes = [
+  { path: '', component: PlannerComponent },
+  { path: '**', redirectTo: '/', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
