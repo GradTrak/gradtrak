dfe511d539bb23000bb27af19b6f738f9f1d5781
Bryan Wu <bryanwu@berkeley.edu>
2020-05-27 15:34:26 -0700

    Add method of comparing semesters

--- client/src/app/planner/components/semester-changer/semester-changer.component.ts
+++ client/src/app/planner/components/semester-changer/semester-changer.component.ts
@@ -45,7 +45,33 @@ export class SemesterChangerComponent implements OnInit {
     this.semesters.push(newSemester);
     this.closeSemesterAdder(); // optional. We can decide if this is needed.
   }
-
+  /**
+  * A helper function that is used to compare two semesters, S1, and S2.
+  * semesters that are earlier in time will be smaller than semesters
+  * that are later in time. Assumes that semesters are named in the format of
+  * "Season 20xx" where season is fall, spring, or summer.
+  * @return a numerical value representing the diff.
+  */
+  const semesterCompare = (s1, s2) => {
+    if (!(s1.includes(' ') || s2.includes(' '))) {
+      console.err('a semester is not properly formatted');
+      return 0;
+    }
+    const calcValue (sem) => {
+      const arr = sem.split(' ');
+      if (arr.length != 2) {
+        console.err('a semester is not properly formatted');
+        return 0; //don't know what else I can do here. Crash the app?
+      }
+      const seasonVal = ({ //sorry. Just assigning points to each season.
+        'spring' : 0,
+        'summer' : 1,
+        'fall' : 2
+      }[arr[0]]) || 3;
+      return parseInt(arr[1] + seasonVal); //sorry nicholas, it's a good meme ok
+    }
+    return (calcValue(s1) - calcValue(s2));
+  }
   removeSemester(semester: Semester): void {
     const index = this.semesters.indexOf(semester);
     this.semesters.splice(index, 1);
