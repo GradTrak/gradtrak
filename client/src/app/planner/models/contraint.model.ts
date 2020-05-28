099fb84ae02ac082216e98da6cd27afc47c39dcf
Nicholas Ngai <ngai.nicholas@protonmail.com>
2020-05-27 21:14:26 -0700

    Add generic constructor to Constraint

--- client/src/app/planner/models/contraint.model.ts
+++ client/src/app/planner/models/contraint.model.ts
@@ -8,6 +8,15 @@ import { Requirement } from './requirement.model';
  */
 export abstract class Constraint {
   /**
+   * Constructs a new Constraint from its prototype.
+   *
+   * @param {ConstraintPrototype} proto The prototype.
+   */
+  constructor(proto: ConstraintPrototype) {
+    Object.assign(this, proto);
+  }
+
+  /**
    * Returns whether the given mapping of requirements to courses is valid.
    *
    * @param {Map<Requirement, Set<Course>>} mapping The mapping of requirement to courses.
