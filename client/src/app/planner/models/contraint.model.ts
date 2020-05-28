46c0c79738a4052ad37dfd49be2bbce135ddfc03
Nicholas Ngai <ngai.nicholas@protonmail.com>
2020-05-27 21:06:58 -0700

    Rewrite Constraint to use isValidMapping interface

--- client/src/app/planner/models/contraint.model.ts
+++ client/src/app/planner/models/contraint.model.ts
@@ -1,51 +1,17 @@
 import { ConstraintPrototype } from 'common/prototypes/constraint.prototype';
-import { RequirementPrototype } from './requirement.prototype';
-import { RequirementSetPrototype } from './requirement-set.prototype';
-import { RequirementCategoryPrototype } from './requirement-category.prototype';
+import { Course } from './course.model';
+import { Requirement } from './requirement.model';
 
 /**
- * The Constraint class represents contraintson a {@link Requirement}s or {@link Requirement-Set}s or {@link Requirement-Category}s.
+ * The Constraint class describes a restriction placed upon ways that {@link Course}s can be assigned to
+ * {@link Requirement}s.
  */
-export class Constraint {
-  requirements: Set<RequirementPrototype>;
-  requirementSets: Set<RequirementSetPrototype>;
-  requirementCategories: Set<RequirementCategoryPrototype>;
-
-  constructor(proto: ConstraintPrototype) {
-    this.requirements = proto.requirements;
-    this.requirementSets = proto.requirementSets;
-    this.requirementCategories = proto.requirementCategories;
-  }
-
-  getRequirement(): Set<RequirementPrototype> {
-    return this.requirements;
-  }
-
-  isApplicable(candidate: Object): boolean {
-    return (this.requirements.contains(candidate) ||
-    this.requirementSets.contains(candidate)||
-    this.requirementCategories.contains(candidate));
-  }
-
+export abstract class Constraint {
   /**
-  * Given a list of requirements, requirementSets, or requirementCategories
-  * to which this constraint might apply, determines if the specific case given will
-  */
-  checkConstraint(course, requirement: Requirement, RequirementSet, or RequirementCategory, mapping): boolean {
-    if (exec(constraint.))
-    return
-  }
+   * Returns whether the given mapping of requirements to courses is valid.
+   *
+   * @param {Map<Requirement, Set<Course>>} mapping The mapping of requirement to courses.
+   * @return {boolean} Whether the mapping is valid.
+   */
+  abstract isValidMapping(mapping: Map<Requirement, Set<Course>>): boolean;
 }
-
-
-/*
-mapping {
-  bioe: cs189, bio134, bioe133
-  eecs: cs170, ...
-}
-course = cs189
-'if mapping.bioe.includes(course){
-  return calculateOverlap(bioe, eecs) > 5
-}'
-
-*/
