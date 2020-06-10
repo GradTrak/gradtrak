9c1f0e406494b0fed288129038e2fdb1fcfb8bd0
Bryan Wu <bryanwu@berkeley.edu>
2020-06-09 22:41:15 -0700

    kill outdated function for comparing semesters for sorting

--- client/src/app/planner/components/semester-changer/semester-changer.component.ts
+++ client/src/app/planner/components/semester-changer/semester-changer.component.ts
@@ -85,8 +85,7 @@ export class SemesterChangerComponent implements OnInit {
     const semesterName: string = season + ' ' + yearNum;
     if (
       this.getSemArr(this.semesters)
-        .map((semester) => semester.name)
-        .includes(semesterName)
+      .some((semester: Semester) => semester.name === semesterName)
     ) {
       this.errorMessage = 'This semester is already in your schedule!';
       return;
@@ -102,38 +101,6 @@ export class SemesterChangerComponent implements OnInit {
       this.semesters.get(academicYearName)[index] = newSemester;
     }
     this.closeSemesterAdder(); // optional. We can decide if this is needed.
-  }
-
-  /**
-   * A helper function that is used to compare two semesters, S1, and S2.
-   * semesters that are earlier in time will be smaller than semesters
-   * that are later in time. Assumes that semesters are named in the format of
-   * "Season 20xx" where season is fall, spring, or summer.
-   * @return a numerical value representing the diff.
-   */
-  semesterCompare(sem1: Semester, sem2: Semester): number {
-    const s1 = sem1.name;
-    const s2 = sem2.name;
-    if (!(s1.includes(' ') || s2.includes(' '))) {
-      console.error('a semester is not properly formatted');
-      return 0;
-    }
-    const calcValue = (sem): number => {
-      const arr = sem.split(' ');
-      if (arr.length !== 2) {
-        console.error('a semester is not properly formatted');
-        return 0; // don't know what else I can do here. Crash the app?
-      }
-      const seasonVal =
-        {
-          // sorry. Just assigning points to each season.
-          Spring: '0',
-          Summer: '1',
-          Fall: '2',
-        }[arr[0]] || '3';
-      return arr[1] + seasonVal - 0; // sorry nicholas, it's a good meme ok
-    };
-    return calcValue(s1) - calcValue(s2);
   }
 
   removeSemester(semester: Semester): void {
