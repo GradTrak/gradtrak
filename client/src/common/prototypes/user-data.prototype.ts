117f2223149216c11cf1f4272410e1b20355c97c
Bryan Wu <bryanwu@berkeley.edu>
2020-06-09 23:22:56 -0700

    revert change from map to object due to property not having map

--- client/src/common/prototypes/user-data.prototype.ts
+++ client/src/common/prototypes/user-data.prototype.ts
@@ -1,7 +1,7 @@
 import { SemesterPrototype } from './semester.prototype';
 
 export interface UserDataPrototype {
-  semesters: any; // eslint-disable-line @typescript-eslint/no-explicit-any
+  semesters: Map<string, SemesterPrototype[]>;
   goalIds: string[];
   manuallyFulfilledReqs: object;
 }
