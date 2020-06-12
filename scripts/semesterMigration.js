5e26012e4af44cabaeae485a8563ac28c7d3dd1d
Bryan Wu <bryanwu@berkeley.edu>
2020-06-11 17:50:26 -0700

    write script for database migration

--- scripts/semesterMigration.js
+++ scripts/semesterMigration.js
@@ -3,10 +3,49 @@ const RequirementSet = require('../server/models/requirement-set');
 const Tag = require('../server/models/tag');
 const User = require('../server/models/user');
 
+
+const seasonVal = {
+  'Spring' : 1,
+  'Summer' : 2,
+  'Fall' : 0,
+}
+
+const findAcadYear = semester => {
+  const semArr = semester.name.split(' ');
+  const semesterYear = parseInt(semArr[1], 10) - (semArr[0] !== 'Fall' ? 1 : 0); // this feels so incredibly clunky.
+  return `${semesterYear.toString()}-${(semesterYear + 1).toString()}`;
+}
 const db = require('../server/config/db');
 db.connect()
   .then((c) => {
-    //TODO: Query everything and then modify their semesters and then dump it back in. 
+    const conn = c;
+    return User.find();
+    //TODO: Query everything and then modify their semesters and then dump it back in.
+  }).then((users) => {
+    console.log();console.log();console.log();console.log();console.log();console.log();console.log();
+
+    const newUsers = users.forEach(user => {
+      let semesterArr = [].concat(...user.userdata.semesters.values())
+      const semesters = new Map();
+      console.log(semesterArr)
+      semesterArr.forEach((semester) => {
+        const academicYearName = findAcadYear(semester);
+        const semArr = semester.name.split(' ');
+        const index = seasonVal[semArr[0]];
+        if (semesters.get(academicYearName)) {
+          semesters.get(academicYearName)[index] = semester;
+        } else {
+          semesters.set(academicYearName, [null, null, null]);
+          semesters.get(academicYearName)[index] = semester;
+        }
+      });
+
+      user.userdata.semesters = semesters
+      console.log(semesters)
+      user.save();
+    });
+
+    return conn;
   })
   .catch((err) => {
     console.error(err);
