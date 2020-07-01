ed994d1ec977ea1e5cae0a38344155372521d483
Nicholas Ngai <ngai.nicholas@protonmail.com>
2020-07-01 08:00:53 -0700

    Use node-fetch and async/await for berkeleyTime.js

--- scripts/berkeleyTime.js
+++ scripts/berkeleyTime.js
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
 
+const fetch = require('node-fetch');
 const fs = require('fs');
 const https = require('https');
 
@@ -35,129 +36,94 @@ const agent = new https.Agent({
   keepAlive: true,
 });
 
-function fetchCourse(courseId) {
-  return new Promise((resolve, reject) => {
-    if (fs.existsSync(`cache/${courseId}.json`)) {
-      resolve({
-        cached: true,
-        data: fs.readFileSync(`cache/${courseId}.json`),
-      });
-      return;
-    }
-
+async function fetchCourse(courseId) {
+  if (fs.existsSync(`cache/${courseId}.json`)) {
+    return JSON.parse(fs.readFileSync(`cache/${courseId}.json`));
+  } else {
     const courseUrl = COURSE_ENDPOINT + courseId;
-    https.get(courseUrl, { agent: agent }, (res) => {
-      let rawData = '';
-
-      res.on('data', (d) => {
-        rawData += d;
-      });
-
-      res.on('end', () => {
-        resolve({
-          cached: false,
-          data: rawData,
-        });
-      });
-    });
-  }).then((result) => {
-    let course = JSON.parse(result.data);
-
-    if (!result.cached) {
-      fs.writeFile(`cache/${courseId}.json`, result.data, (err) => {
-        if (err) {
-          console.error(err);
-        }
-      });
-    }
-
-    return course;
-  });
-}
-
-async function fetchCourseTags(courses) {
-  for (let i = 0; i < courses.length; i++) {
-    const course = courses[i];
-
-    course.tagIds = await fetchCourse(course._id)
-      .then((courseData) => {
-        return courseData.requirements
-          .filter((reqName) => Array.from(TAG_MAP.keys()).includes(reqName))
-          .map((reqName) => TAG_MAP.get(reqName));
-      })
-      .catch((err) => {
-        console.error(`Error getting course with ID ${course._id}:`);
+    const res = await fetch(courseUrl, { agent });
+    const data = await res.json();
+    fs.writeFile(`cache/${courseId}.json`, JSON.stringify(data), (err) => {
+      if (err) {
         console.error(err);
-        return [];
-      });
-
-    delete course._id;
-
-    if (i % 100 === 0) {
-      console.log(`Done: ${i} / ${courses.length}`);
-    }
+      }
+    });
+    return data;
   }
 }
 
-https.get(LIST_ENDPOINT, { agent: agent }, (res) => {
-  let rawData = '';
+async function fetchCourseTags(course) {
+  try {
+    const courseData = await fetchCourse(course._id);
+    return courseData.requirements
+      .filter((reqName) => Array.from(TAG_MAP.keys()).includes(reqName))
+      .map((reqName) => TAG_MAP.get(reqName));
+  } catch (err) {
+    console.error(`Error getting course with ID ${course._id}:`);
+    console.error(err);
+    return [];
+  }
+}
 
-  res.on('data', (d) => {
-    rawData += d;
+(async () => {
+  const res = await fetch(LIST_ENDPOINT, { agent: agent });
+  const data = await res.json();
+
+  let validCourses = data.filter(
+    (course) => course.units && (course.units.match(/^\d+\.\d+$/) || course.units.match(/^\d+$/)),
+  );
+  const courseMap = new Map();
+
+  validCourses.forEach((course) => {
+    delete course.open_seats;
+    delete course.description;
+    delete course.enrolled_percentage;
+    delete course.favorite_count;
+    delete course.waitlisted;
+    delete course.enrolled;
+    delete course.grade_average;
+    delete course.letter_average;
+
+    course.dept = course.abbreviation;
+    course.no = course.course_number;
+    delete course.abbreviation;
+    delete course.course_number;
+
+    course._id = course.id;
+    course.id = (course.dept + course.no).replace(/[^A-Za-z\d]/, '').toLowerCase();
+
+    // Convert units to number
+    course.units = parseFloat(course.units);
+
+    if (courseMap.has(course.id)) {
+      console.error(`Warning: Course with id ${course.id} has conflict`);
+    } else {
+      courseMap.set(course.id, course);
+    }
   });
+  validCourses = Array.from(courseMap.values());
 
-  res.on('end', () => {
-    const data = JSON.parse(rawData);
+  for (let i = 0; i < validCourses.length; i++) {
+    const course = validCourses[i];
+    const tags = await fetchCourseTags(course);
+    course.tagIds = tags;
+    delete course._id;
+    if (i % 100 === 0) {
+      console.log(`Done: ${i} / ${validCourses.length}`);
+    }
+  }
 
-    const validCourses = data.filter(
-      (course) => course.units && (course.units.match(/^\d+\.\d+$/) || course.units.match(/^\d+$/)),
-    );
+  validCourses.sort((a, b) => {
+    if (a.dept === b.dept) {
+      const aNo = parseInt(a.no.replace(/[^\d]/g, ''));
+      const bNo = parseInt(b.no.replace(/[^\d]/g, ''));
 
-    const courseMap = new Map();
-
-    validCourses.forEach((course) => {
-      delete course.open_seats;
-      delete course.description;
-      delete course.enrolled_percentage;
-      delete course.favorite_count;
-      delete course.waitlisted;
-      delete course.enrolled;
-      delete course.grade_average;
-      delete course.letter_average;
-
-      course.dept = course.abbreviation;
-      course.no = course.course_number;
-      delete course.abbreviation;
-      delete course.course_number;
-
-      course._id = course.id;
-      course.id = (course.dept + course.no).replace(/[^A-Za-z\d]/, '').toLowerCase();
-
-      // Convert units to number
-      course.units = parseFloat(course.units);
-
-      if (courseMap.has(course.id)) {
-        console.error(`Warning: Course with id ${course.id} has conflict`);
-      } else {
-        courseMap.set(course.id, course);
+      if (aNo === bNo) {
+        return a.no > b.no ? 1 : -1;
       }
-    });
-
-    fetchCourseTags(validCourses).then(() => {
-      const courses = Array.from(courseMap.values());
-      courses.sort((a, b) => {
-        if (a.dept === b.dept) {
-          const aNo = parseInt(a.no.replace(/[^\d]/g, ''));
-          const bNo = parseInt(b.no.replace(/[^\d]/g, ''));
-
-          if (aNo === bNo) {
-            return a.no > b.no ? 1 : -1;
-          }
-          return aNo > bNo ? 1 : -1;
-        }
-        return a.dept > b.dept ? 1 : -1;
-      });
-      fs.writeFileSync(DUMMY_COURSE_DATA, JSON.stringify(courses));
-    });
+      return aNo > bNo ? 1 : -1;
+    }
+    return a.dept > b.dept ? 1 : -1;
   });
-});
+  fs.writeFileSync(DUMMY_COURSE_DATA, JSON.stringify(validCourses));
+})();
