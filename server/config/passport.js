25ffc873b60e91ef9c96076c1b087518d83c45de
Nicholas Ngai <ngai.nicholas@protonmail.com>
2020-06-30 18:34:47 -0700

    Check for empty password hash before verifying password

--- server/config/passport.js
+++ server/config/passport.js
@@ -5,7 +5,7 @@ const { Strategy } = require('passport-local');
 const User = require('../models/user');
 
 async function verifyUser(user, inputPassword) {
-  if (!user) {
+  if (!user || !user.passwordHash) {
     /* Verify dummy password to prevent timing attack enumerating users */
     /* This is the hash of 'password' if anyone is curious */
     const dummy = '$argon2id$v=19$m=4096,t=3,p=1$bPaz0G0LK/r5aSoCQsU8Bg$8mbAff+svZA0QV3XH5i5vSPTYBE5xd4rmVZEDF0umvA';
