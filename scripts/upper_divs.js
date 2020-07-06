8d31abcc988502674fb5d0c2b991c673fda3aa2e
Bryan Wu <bryanwu@berkeley.edu>
2020-07-05 23:58:10 -0700

    tag statistic electives

--- scripts/upper_divs.js
+++ scripts/upper_divs.js
@@ -1,15 +1,16 @@
 const data = require('./dummy/berkeleyTime.json');
 const fs = require('fs')
 data.forEach((course) => {
-  if (course.no >= 100 && course.no <=196 && !course.title.includes('Special Topic')) {
+  const no = course.no.replace(/[^0-9\.]+/g, "");
+  if (no >= 100 && no <=196 && course.dept === 'ECON') {
     if (!course.tagIds.includes(course.tagIds)) {
-      course.tagIds = [...course.tagIds, 'upper_div'];
+      course.tagIds = [...course.tagIds, 'economics_upper_div'];
     }
   }
 });
 const saveString = JSON.stringify(data, null, 2);
 fs.writeFile('dummy/berkeleyTime.json', saveString, (error) => {
-  if (err) {
+  if (error) {
     console.error(error)
   }
 })
