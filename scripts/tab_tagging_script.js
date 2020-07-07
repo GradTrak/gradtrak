cf9c59a7cb125c70632720b315b237a9daa28db7
Bryan Wu <bryanwu@berkeley.edu>
2020-07-06 21:50:34 -0700

    create a tagging script

---
+++ scripts/tab_tagging_script.js
@@ -0,0 +1,46 @@
+const data = require('./dummy/berkeleyTime.json');
+const fs = require('fs');
+const flatmap = require('flatmap'); //YOU WILL NEED TO INSTALL FLATMAP TO USE THIS.
+const hasNumber = /\d/g;
+const nonAlphanumeric = /[^a-zA-Z0-9]/;
+
+
+
+//INPUTS
+const MY_FILE = './bioeTechTopics.txt'; //change to whatever
+const TAG = 'bioeng_technical_topics';
+
+
+
+var eligibleCourses = fs.readFileSync(MY_FILE).toString()
+eligibleCourses = eligibleCourses.split('\n')
+eligibleCourses = eligibleCourses.map(line => line.split('\t')[0].split(' / ').join('/'));
+eligibleCourses = flatmap(eligibleCourses, courses => {
+  const splitCourses = courses.split('/');
+  if (splitCourses.length === 1) {
+    return splitCourses
+  }
+  if (hasNumber.test(splitCourses[0])) { //COMPSCI C100/DATA C100
+    return splitCourses;
+  }
+  else { //COMPSCI/DATA/STAT C100
+    //TODO
+  }
+});
+eligibleCourses = eligibleCourses.map(courseId => courseId.replace(nonAlphanumeric, ''))
+eligibleCourses = eligibleCourses.map(courseId => courseId.replace(/\s/, ''))
+data.forEach((course) => {
+  const no = course.no.replace(/[a-zA-Z,]/, "");
+  if (eligibleCourses.includes(course.id.toLowerCase())) {
+    if (!course.tagIds.includes(TAG)) {
+      console.log(course.id)
+      course.tagIds = [...course.tagIds, TAG];
+    }
+  }
+});
+const saveString = JSON.stringify(data, null, 2);
+fs.writeFile('dummy/berkeleyTime.json', saveString, (error) => {
+  if (error) {
+    console.error(error)
+  }
+})
--- scripts/tagging_script.js
+++ scripts/tagging_script.js
@@ -1,13 +1,27 @@
 const data = require('./dummy/berkeleyTime.json');
 const fs = require('fs')
-var techTopics = fs.readFileSync('./bioeTechTopics.txt').toString()
-techTopics = techTopics.split('\n').map(line => line.split(' '))
-techTopics = techTopics.map(line => (line[0] + line[1]).toLowerCase());
-//console.log(techTopics);
+const MY_FILE = './bioeeligibleCourses.txt'; //change to whatever
+
+
+
+var eligibleCourses = fs.readFileSync(MY_FILE).toString()
+eligibleCourses = eligibleCourses.split('\n').map(line => line.split(' '))
+eligibleCourses = eligibleCourses.flatmap(line => {
+  var regex = /\d/g;
+  if (regex.test(line[1])) { //if the dept name is one word
+    return (line[0] + line[1]).toLowerCase()
+  }
+  else if (regex.test(line[2])) {
+    return (line[0] + line[1] + line[2]).toLowerCase()
+  } else {
+    console.warn("HEEEEELLPP THIS IS POORLY PARSED!");
+  }
+});
+//console.log(eligibleCourses);
 //process.exit(1)
 data.forEach((course) => {
   const no = course.no.replace(/[a-zA-Z]/, "");
-  if (techTopics.includes(course.id.toLowerCase())) {
+  if (eligibleCourses.includes(course.id.toLowerCase())) {
     if (!course.tagIds.includes('bioeng_technical_topics')) {
       console.log(course.id)
       course.tagIds = [...course.tagIds, 'bioeng_technical_topics'];
