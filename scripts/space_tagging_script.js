0f7cc20b338d18a2aaa680070a62f144acb0fa2f
Bryan Wu <bryanwu@berkeley.edu>
2020-07-06 23:31:53 -0700

    ignore bioe100

--- scripts/space_tagging_script.js
+++ scripts/space_tagging_script.js
@@ -1,9 +1,9 @@
 const data = require('./dummy/berkeleyTime.json');
 const fs = require('fs')
-const MY_FILE = './tagging_lists/' + 'foreign_language.txt'; //change to whatever
+const MY_FILE = './tagging_lists/' + 'ls_techelect.txt'; //change to whatever
 const TAG = 'cs_technical_elective';
 
-/*
+
 var eligibleCourses = fs.readFileSync(MY_FILE).toString();
 eligibleCourses = eligibleCourses.split('\n').map(line => line.split(' '))
 eligibleCourses = eligibleCourses.map(line => {
@@ -17,14 +17,14 @@ eligibleCourses = eligibleCourses.map(line => {
     console.warn("HEEEEELLPP THIS IS POORLY PARSED!:",  line);
   }
 }).filter(a => a);
-*/
-//console.log(eligibleCourses);
-//process.exit(1)
+
+console.log(eligibleCourses);
+process.exit(1)
 
 const allowed = ['BIO ENG', 'CHEM', 'CHM ENG', 'CIV ENG', 'MATH', 'EL ENG', 'ING ENG', 'MECH ENG', 'MCELLBI', 'PHYSICS', 'STAT']//'ASTRON', 'CHEM', 'EPS', 'INTEGBI', 'MCELLBI', 'PHYSICS', 'PLANTBI']
 data.forEach((course) => {
-  //if (eligibleCourses.includes(course.id.toLowerCase())) {
-  if (allowed.includes(course.dept) && course.no.replace(/[A-Za-z]/g, '') >= 100/*&& course.no.replace(/[A-Za-z]/g, '') < 200*/) {
+  if (eligibleCourses.includes(course.id.toLowerCase())) {
+  //if (allowed.includes(course.dept) && course.no.replace(/[A-Za-z]/g, '') >= 100/*&& course.no.replace(/[A-Za-z]/g, '') < 200*/) {
     if (!course.tagIds.includes(TAG)) {
       console.log(course.id)
       course.tagIds = [...course.tagIds, TAG];
