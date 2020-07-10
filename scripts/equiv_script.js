1630a3e92e85b5c18ffb8faa94d369aeeef88f25
Bryan Wu <bryanwu@berkeley.edu>
2020-07-10 00:07:46 -0700

    add equiv for w courses

--- scripts/equiv_script.js
+++ scripts/equiv_script.js
@@ -1,26 +1,35 @@
 const data = require('./dummy/berkeleyTime.json');
 const fs = require('fs')
 const MY_FILE = './tagging_lists/' + 'fpf.txt'; //change to whatever
-const TAG = 'rc_a';
+const processEligibleCourses = false;
+const courseArr = data.map(course => course.no);
+const IS_EQUIV = (course) => {
+  return course.no.toLowerCase()[0] === 'w' && courseArr.includes(course.no.toLowerCase().slice(1));
+}
 
-///*
-var eligibleCourses = fs.readFileSync(MY_FILE).toString();
-eligibleCourses = eligibleCourses.split('\n').map(line => line.split(' '))
-eligibleCourses = eligibleCourses.map(line => {
-  var regex = /\d/g;
-  if (regex.test(line[1])) { //if the dept name is one word
-    return (line[0] + line[1]).toLowerCase()
-  }
-  else if (regex.test(line[2])) {
-    return (line[0] + line[1] + line[2]).toLowerCase()
-  } else {
-    console.warn("HEEEEELLPP THIS IS POORLY PARSED!:",  line);
-  }
-}).filter(a => a);
+let eligibleCourses;
+if (processEligibleCourses){
+  eligibleCourses = fs.readFileSync(MY_FILE).toString();
+  eligibleCourses = eligibleCourses.split('\n').map(line => line.split(' '))
+  eligibleCourses = eligibleCourses.map(line => {
+    var regex = /\d/g;
+    if (regex.test(line[1])) { //if the dept name is one word
+      return (line[0] + line[1]).toLowerCase()
+    }
+    else if (regex.test(line[2])) {
+      return (line[0] + line[1] + line[2]).toLowerCase()
+    } else {
+      console.warn("HEEEEELLPP THIS IS POORLY PARSED!:",  line);
+    }
+  }).filter(a => a);
+
+  console.log(eligibleCourses);
+  process.exit(1)
+}
 
 data.forEach((course) => {
-  if (eligibleCourses.includes(course.id.toLowerCase())) {
-    const courseName = course.id.slice(1)
+  if (IS_EQUIV(course)) {
+    const courseName = course.id.replace(course.no.toLowerCase(), course.no.toLowerCase().slice(1) )
     course.equivIds = course.equivIds || []
     if (!course.equivIds.includes(courseName)) {
       console.log(course.id)
@@ -28,6 +37,7 @@ data.forEach((course) => {
     }
   }
 });
+//process.exit();
 let saveString = JSON.stringify(data, null, 2);
 fs.writeFileSync('dummy/berkeleyTime.json', saveString, (error) => {
   if (error) {
