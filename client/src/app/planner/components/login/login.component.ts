c2133a3fad1a975a811122b10b23d7bc14ae2511
Danial Khan <danialk1@berkeley.edu>
2020-07-13 10:40:51 -0700

    get rid of the console statements (oops)

--- client/src/app/planner/components/login/login.component.ts
+++ client/src/app/planner/components/login/login.component.ts
@@ -86,9 +86,6 @@ export class LoginComponent implements OnInit {
 
     this.loading = true;
     this.regEmailMarketing = true;
-    console.log(this.username);
-    console.log(this.regEmailMarketing);
-    console.log(this.regUserTesting);
 
     this.userService
       .register(this.username, this.password, this.regEmailMarketing, this.regUserTesting)
