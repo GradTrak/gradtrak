40e338c6e3101078bcfe8004bf413a215c51d375
Bryan Wu <bryanwu@berkeley.edu>
2020-08-09 18:54:42 -0700

    add base countrequirement model

---
+++ client/src/app/planner/models/requirements/count-requirement.model.ts
@@ -0,0 +1,47 @@
+import { Course } from '../course.model';
+import { Requirement } from '../requirement.model';
+import { StandaloneRequirement } from './standalone-requirement.model';
+
+import { getAllCombinations } from '../../../../utils';
+
+/**
+ * The CountRequiremenet class represents a {@link Requirement}, has one base {@link StandaloneRequirement} and is only
+ * fulfilled if a certain number of classes fulfills the the base.
+ */
+export class CountRequirement extends Requirement {
+  count: number;
+  requirement: StandaloneRequirement;
+
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  isFulfilledWith(courses: Course[], override?: Set<string>): boolean {
+    return this.unitsFulfilled(courses) >= this.units;
+  }
+  /**
+   * The number of courses that can fulfill the requirement
+   */
+  numFulfilled(courses: Course[]): number {
+    return this.getFulfillingCourses(courses).length;
+  }
+
+  /**
+   * Any COURSE that is a possible contribution to any child requirement is
+   * a possible contribution to a UnitRequirement, and will return true.
+   * @return whether the course could possibly help fullfill a requirement.
+   */
+  canFulfill(course: Course): boolean {
+    return this.requirement.canFulfill(course);
+  }
+
+  getFulfillingCourses(courses: Course[]): Course[] {
+    return courses.filter((course) => this.requirement.isFulfilled(course));
+  }
+
+  getCourseCombinations(courses: Course[]): Set<Course>[] {
+    const filteredCourses: Course[] = courses.filter((course: Course) => this.requirement.isFulfilledWith([course]));
+    return getAllCombinations(filteredCourses).map((combination: Course[]) => new Set<Course>(combination));
+  }
+
+  toString(): string {
+    return `${this.count} courses of \n${this.requirement.name}`;
+  }
+}
