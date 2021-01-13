ceef44fb1c43c81a91e9dbfe61a57fcc28803fea
Nicholas Ngai <ngai.nicholas@protonmail.com>
2021-01-13 08:52:45 -0800

    Migrate root component base HTML template and logic to React

--- client-react/src/App.tsx
+++ client-react/src/App.tsx
@@ -3,11 +3,26 @@ import { Course } from './models/course.model';
 import { Requirement } from './models/requirement.model';
 import { RequirementSet } from './models/requirement-set.model';
 import { Semester } from './models/semester.model';
+import { StandaloneRequirement } from './models/requirements/standalone-requirement.model';
 import { UserData } from './models/user-data.model';
 import User, { AuthType } from './lib/user';
 import logo from './logo.svg';
 import './App.css';
+
 type AppProps = {};
+
+type ModalState =
+  | {
+      type: 'login' | 'initializer' | 'account-editor' | 'report-form' | 'goal-selector' | 'semester-changer';
+    }
+  | {
+      type: 'course-adder';
+      semester: Semester;
+    }
+  | {
+      type: 'requirement-display';
+      requirement: StandaloneRequirement;
+    };
 
 type AppState = {
   loggedIn: boolean;
@@ -15,8 +30,9 @@ type AppState = {
     username: string;
     auth: AuthType;
   };
-
   userData: UserData;
+  isLoading: boolean;
+  modal: ModalState;
 };
 
 class App extends React.Component<AppProps, AppState> {
@@ -34,10 +50,91 @@ class App extends React.Component<AppProps, AppState> {
         goals: [],
         manuallyFulfilledReqs: new Map<string, Set<string>>(),
       },
+      isLoading: true,
+      modal: null,
     };
   }
 
-  async register(username: string, password: string, userTesting: boolean): Promise<boolean> {
+  closeModal = (): void => {
+    this.setState({
+      modal: null,
+    });
+  };
+
+  openLogin = (): void => {
+    this.setState({
+      modal: {
+        type: 'login',
+      },
+    });
+  };
+
+  onLoginDismiss = (): void => {
+    this.closeModal();
+    if (this.state.userData.semesters.size === 0) {
+      this.openInitializer();
+    }
+  };
+
+  openInitializer = (): void => {
+    this.setState({
+      modal: {
+        type: 'initializer',
+      },
+    });
+  };
+
+  openAccountEditor = (): void => {
+    this.setState({
+      modal: {
+        type: 'account-editor',
+      },
+    });
+  };
+
+  openReportForm = (): void => {
+    this.setState({
+      modal: {
+        type: 'report-form',
+      },
+    });
+  };
+
+  openGoalSelector = (): void => {
+    this.setState({
+      modal: {
+        type: 'goal-selector',
+      },
+    });
+  };
+
+  openSemesterChanger = (): void => {
+    this.setState({
+      modal: {
+        type: 'semester-changer',
+      },
+    });
+  };
+
+  openCourseAdder = (semester: Semester): void => {
+    this.setState({
+      modal: {
+        type: 'course-adder',
+        semester,
+      },
+    });
+  };
+
+  openRequirementDisplay = (requirement: StandaloneRequirement): void => {
+    this.setState({
+      modal: {
+        type: 'requirement-display',
+        requirement,
+      },
+    });
+  };
+
+  register = async (username: string, password: string, userTesting: boolean): Promise<boolean> => {
     if (this.state.loggedIn) {
       throw new Error('Tried to register when already logged in');
     }
@@ -54,9 +151,9 @@ class App extends React.Component<AppProps, AppState> {
       });
     }
     return res.success;
-  }
+  };
 
-  async login(username: string, password: string): Promise<boolean> {
+  login = async (username: string, password: string): Promise<boolean> => {
     if (this.state.loggedIn) {
       throw new Error('Tried to register when already logged in');
     }
@@ -73,12 +170,12 @@ class App extends React.Component<AppProps, AppState> {
       });
     }
     return res.success;
-  }
+  };
 
   /**
    * Logs the user out of the application.
    */
-  async logout(): Promise<void> {
+  logout = async (): Promise<void> => {
     if (!this.state.loggedIn) {
       throw new Error('Tried to log out when not logged in');
     }
@@ -89,9 +186,9 @@ class App extends React.Component<AppProps, AppState> {
       loggedIn: false,
       user: null,
     });
-  }
+  };
 
-  async queryWhoami(): Promise<string> {
+  queryWhoami = async (): Promise<string> => {
     const res = await User.whoami();
     if (res.loggedIn) {
       this.setState({
@@ -110,18 +207,18 @@ class App extends React.Component<AppProps, AppState> {
       });
     }
     return res.loggedIn ? res.username : null;
-  }
+  };
 
-  async changePassword(oldPassword: string, newPassword: string): Promise<boolean> {
+  changePassword = async (oldPassword: string, newPassword: string): Promise<boolean> => {
     if (!this.state.loggedIn || this.state.user.auth !== 'local') {
       throw new Error('Tried to change password in invalid state');
     }
 
     const res = await User.changePassword(oldPassword, newPassword);
     return Boolean(res.error);
-  }
+  };
 
-  async fetchUserData(): Promise<void> {
+  fetchUserData = async (): Promise<void> => {
     const userData = await User.fetchUserData();
     if (userData.semesters.size > 0) {
       this.setState({
@@ -129,14 +226,14 @@ class App extends React.Component<AppProps, AppState> {
         userData,
       });
     }
-  }
+  };
 
   /**
    * Updates the list of semesters to a new mapping of given semesters.
    *
    * @param {Map<string, Semester[]>} newSemesters The new semesters.
    */
-  updateSemesters(newSemesters: Map<string, Semester[]>): void {
+  updateSemesters = (newSemesters: Map<string, Semester[]>): void => {
     // not sure why, before the rework the list of semesters was copied, so
     // I've copied the map here as well.
     const newMap = new Map<string, Semester[]>();
@@ -150,14 +247,14 @@ class App extends React.Component<AppProps, AppState> {
         semesters: newMap,
       },
     });
-  }
+  };
 
   /**
    * Updates the list of goals to a new list of given goals.
    *
    * @param {RequirementSet[]} newGoals The new goals.
    */
-  updateGoals(newGoals: RequirementSet[]): void {
+  updateGoals = (newGoals: RequirementSet[]): void => {
     this.setState({
       ...this.state,
       userData: {
@@ -165,7 +262,7 @@ class App extends React.Component<AppProps, AppState> {
         goals: [...newGoals],
       },
     });
-  }
+  };
 
   /**
    * Adds a course to a given semester.
@@ -173,7 +270,7 @@ class App extends React.Component<AppProps, AppState> {
    * @param {Course} course The course to add.
    * @param {Semester} semester The semester to which to add the course.
    */
-  addCourse(course: Course, semester: Semester): void {
+  addCourse = (course: Course, semester: Semester): void => {
     if (semester.courses.includes(course)) {
       console.error(`Tried to add course ${course.id} to semester ${semester.name}, which it already has`);
       return;
@@ -189,7 +286,7 @@ class App extends React.Component<AppProps, AppState> {
         semesters: new Map<string, Semester[]>(this.state.userData.semesters),
       },
     });
-  }
+  };
 
   /**
    * Removes a course from a given semester.
@@ -197,7 +294,7 @@ class App extends React.Component<AppProps, AppState> {
    * @param {Course} course The course to remove.
    * @param {Semester} semester The semester from which to remove the course.
    */
-  removeCourse(course: Course, semester: Semester): void {
+  removeCourse = (course: Course, semester: Semester): void => {
     if (!semester.courses.includes(course)) {
       console.error(`Tried to remove course ${course.id} from semester ${semester.name}, which it doesn't have`);
       return;
@@ -212,9 +309,9 @@ class App extends React.Component<AppProps, AppState> {
         semesters: new Map<string, Semester[]>(this.state.userData.semesters),
       },
     });
-  }
+  };
 
-  manuallyFulfill(requirement: Requirement, requirementSet: RequirementSet): void {
+  manuallyFulfill = (requirement: Requirement, requirementSet: RequirementSet): void => {
     const { manuallyFulfilledReqs } = this.state.userData;
 
     if (
@@ -240,9 +337,9 @@ class App extends React.Component<AppProps, AppState> {
         manuallyFulfilledReqs: nextManuallyFulfilled,
       },
     });
-  }
+  };
 
-  manuallyUnfulfill(requirement: Requirement, requirementSet: RequirementSet): void {
+  manuallyUnfulfill = (requirement: Requirement, requirementSet: RequirementSet): void => {
     const { manuallyFulfilledReqs } = this.state.userData;
 
     if (
@@ -270,28 +367,122 @@ class App extends React.Component<AppProps, AppState> {
         manuallyFulfilledReqs: nextManuallyFulfilled,
       },
     });
-  }
+  };
 
-  setUserData(userData: UserData): void {
+  setUserData = (userData: UserData): void => {
     this.setState({
       ...this.state,
       userData,
     });
+  };
+
+  /**
+   * Gets a list of all the courses a user is currently taking, in the form of
+   * an array, based on the currrent state and userdata.
+   *
+   * @return {Course[]} A list of courses that are in the state.
+   */
+  private getCurrentCourses = (): Course[] => {
+    return Array.from(this.state.userData.semesters.values())
+      .flat()
+      .filter((semester: Semester) => semester)
+      .flatMap((semester: Semester) => semester.courses);
+  };
+
+  private renderName = (): React.ReactElement => {
+    if (this.state.loggedIn) {
+      return (
+        <>
+          <div className="left">Name: {this.state.user.username}</div>
+          <div className="right">
+            <button className="gt-button" onClick={this.openAccountEditor}>
+              Account
+            </button>
+            <span> | </span>
+            <button className="gt-button" onClick={this.logout}>
+              Logout
+            </button>
+            <span> | </span>
+            <a
+              className="gt-button"
+              href="https://docs.google.com/document/d/1sr9zSIZItja7008yAB5kYDhEZv-1SO0I7zIJtUmZ0Kk/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Help
+            </a>
+            <span> | &nbsp;</span>
+            <i className="material-icons" onClick={this.openReportForm}>
+              flag
+            </i>
+          </div>
+        </>
+      );
+    } else {
+      /* Not logged in. */
+      return (
+        <>
+          <div className="left">Guest User</div>
+          <div className="right">
+            <button className="gt-button" onClick={this.openLogin}>
+              Register to Save
+            </button>
+            <span> | </span>
+            <a
+              className="gt-button"
+              href="https://docs.google.com/document/d/1sr9zSIZItja7008yAB5kYDhEZv-1SO0I7zIJtUmZ0Kk/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Help
+            </a>
+            <span> | &nbsp;</span>
+            <i className="material-icons" onClick={this.openReportForm}>
+              flag
+            </i>
+          </div>
+        </>
+      );
+    }
+  };
+
+  private renderBody(): React.ReactElement {
+    if (!this.state.isLoading) {
+      return (
+        <div className="row no-gutters main">
+          <SemesterPane
+            className="col-8"
+            semesters={this.state.userData.semesters}
+            onOpenSemesterChanger={this.openSemesterChanger()}
+            onOpenCourseAdder={this.openCourseAdder}
+          />
+          <RequirementsPane
+            className="col-4"
+            courses={this.getCurrentCourses()}
+            goals={this.state.userData.goals}
+            manuallyFulfilled={this.state.userData.manuallyFulfilledReqs}
+            onOpenGoalSelector={this.openGoalSelector}
+            onOpenRequirementDisplay={this.openRequirementDisplay}
+          />
+        </div>
+      );
+    } else {
+      /* Loading. */
+      return <div>Loading...</div>;
+    }
   }
 
-  render() {
+  render(): React.ReactElement {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-            Learn React
-          </a>
-        </header>
-      </div>
+      <>
+        <div className="header">
+          <div className="title">
+            GradTrak<sup className="beta">BETA</sup>
+          </div>
+          <div className="name">{this.renderName()}</div>
+        </div>
+        <div className="body">{this.renderBody()}</div>
+      </>
     );
   }
 }
