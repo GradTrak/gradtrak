85e22b69897d00a807a0f17b74784aa5a874374d
Nicholas Ngai <ngai.nicholas@protonmail.com>
2021-01-19 10:09:42 -0800

    Remove webpack config from Babel exclusion list

--- src/client/webpack.common.js
+++ src/client/webpack.common.js
@@ -44,10 +44,7 @@ module.exports = {
       {
         loader: 'babel-loader',
         test: /\.(js|jsx)$/,
-        exclude: [
-          /node_modules/,
-          /^webpack\..*\.js$/,
-        ],
+        exclude: /node_modules/,
       },
       {
         use: ['style-loader', 'css-loader'],
