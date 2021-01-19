df4750d3a9a2d34c0c45b2cd46d63287c4e44ce9
Nicholas Ngai <ngai.nicholas@protonmail.com>
2021-01-19 08:16:03 -0800

    Remove Angular files and rename client-react to client

--- client/src/app/planner/models/requirement-container.model.ts
+++
@@ -1,7 +0,0 @@
-import { Requirement } from './requirement.model';
-/**
- * An interface for anything with multiple requirements in it.
- */
-export interface RequirementContainer {
-  requirements: Requirement[];
-}
