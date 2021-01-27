542b3c1471c71f75abe6ce7666ccc64b77f5399b
Nicholas Ngai <ngai.nicholas@protonmail.com>
2021-01-27 07:28:33 -0800

    Project away schemaVersion field from API

--- src/server/controllers/requirementController.ts
+++ src/server/controllers/requirementController.ts
@@ -7,7 +7,14 @@ import RequirementSet from '../models/requirement-set';
 queries mongo for any requirement models and calls successCallback on what is returned
 */
 async function queryRequirements(): Promise<RequirementSetPrototype[]> {
-  return RequirementSet.find({}, { _id: 0, __v: 0 });
+  return RequirementSet.find(
+    {},
+    {
+      _id: 0,
+      __v: 0,
+      schemaVersion: 0,
+    },
+  );
 }
 
 export async function getRequirements(req: express.Request, res: express.Response): Promise<void> {
